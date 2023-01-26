7a07b8d43139e98068d0d38567128959a8f6d840
Osaro Iyoha <saro.iyoha@gmail.com>
2023-01-26 17:09:23 +0100

    Style SVG

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import arrowIcon from '../images/icon-arrow.svg'
 const AccordionItem = ({title,info}) => {
   const [showInfo, setShowInfo] = useState(false);
+  const softRedColor = '#fa5757';
+  const softBlueColor = '#5267DF';
   return <>
     <div className={`accordion-item ${showInfo && "accordion-item--active"}`} onClick={()=>setShowInfo(!showInfo)}>
       <div className="accordion-item--header">
           <h4>{title}</h4>
-          <button className="accordion-item--header__btn" ><img src={arrowIcon} alt="" /></button>
+          <button className="accordion-item--header__btn" >
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className='arrowIcon'>
+            <path fill="none" stroke={`${showInfo?softRedColor:softBlueColor}`} strokeWidth="3" d="M1 1l8 8 8-8"/>
+          </svg>
+          </button>
       </div>
       {showInfo && <div className="accordion-item--text">
           <p>{info}</p>
